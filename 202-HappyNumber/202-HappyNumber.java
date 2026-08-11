57c9192b238d45b33f092b21381a0968a19a6b14
Afna-MS <afnams4050@gmail.com>
2026-08-11 16:12:37 +0530

    Create 202-HappyNumber

---
+++ 202-HappyNumber/202-HappyNumber.java
@@ -0,0 +1,15 @@
+// Last updated: 11/08/2026, 16:12:35
+class Solution {
+    public boolean isHappy(int n) {
+        while(n!=1 && n!=4){
+            int sum=0;
+            while(n>0){
+                int d=n%10;
+                sum+=d*d;
+                n/=10;
+            }
+            n=sum;
+        }
+        return n==1;
+    }
+}
