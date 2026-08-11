9b5c04f0ce0d8406f18923b8fe196b4021e16d13
Afna-MS <afnams4050@gmail.com>
2026-08-11 16:09:13 +0530

    Create 412-FizzBuzz

---
+++ 412-FizzBuzz/412-FizzBuzz.java
@@ -0,0 +1,13 @@
+// Last updated: 11/08/2026, 16:09:13
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> res = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 15 == 0) res.add("FizzBuzz");
+            else if (i % 3 == 0) res.add("Fizz");
+            else if (i % 5 == 0) res.add("Buzz");
+            else res.add(String.valueOf(i));
+        }
+        return res;
+    }
+}
