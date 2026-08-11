ae2d81224f1cbe509e0080dc16af2882902e069e
Afna-MS <afnams4050@gmail.com>
2026-08-11 16:06:13 +0530

    Create 645-SetMismatch

---
+++ 645-SetMismatch/645-SetMismatch.java
@@ -0,0 +1,24 @@
+// Last updated: 11/08/2026, 16:06:12
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int n = nums.length;
+        boolean[] seen = new boolean[n + 1];
+        int duplicate = -1, missing = -1;
+
+        for (int num : nums) {
+            if (seen[num]) {
+                duplicate = num;
+            }
+            seen[num] = true;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (!seen[i]) {
+                missing = i;
+                break;
+            }
+        }
+
+        return new int[]{duplicate, missing};
+    }
+}
