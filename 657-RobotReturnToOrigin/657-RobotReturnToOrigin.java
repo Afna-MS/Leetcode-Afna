5c36dc140f3e638ba940798733fb2b48051d4ffe
Afna-MS <afnams4050@gmail.com>
2026-08-11 16:06:09 +0530

    Create 657-RobotReturnToOrigin

---
+++ 657-RobotReturnToOrigin/657-RobotReturnToOrigin.java
@@ -0,0 +1,13 @@
+// Last updated: 11/08/2026, 16:06:09
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x = 0, y = 0;
+        for (char c : moves.toCharArray()) {
+            if (c == 'U') y++;
+            else if (c == 'D') y--;
+            else if (c == 'L') x--;
+            else if (c == 'R') x++;
+        }
+        return x == 0 && y == 0;
+    }
+}
