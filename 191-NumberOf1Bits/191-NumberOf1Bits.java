dd49a0c14691c1a4176298525980754ac20e1695
Afna-MS <afnams4050@gmail.com>
2026-08-11 16:12:56 +0530

    Create 191-NumberOf1Bits

---
+++ 191-NumberOf1Bits/191-NumberOf1Bits.java
@@ -0,0 +1,10 @@
+// Last updated: 11/08/2026, 16:12:56
+class Solution {
+    public int hammingWeight(int n) {
+        int c=0;
+        while(n!=0){
+            n=n&(n-1);
+            c++;
+        }return c;
+    }
+}
