771453930633ed5dadefc222eb807caca5799a6b
Afna-MS <afnams4050@gmail.com>
2026-08-11 16:01:02 +0530

    Create 2491-SmallestEvenMultiple

---
+++ 2491-SmallestEvenMultiple/2491-SmallestEvenMultiple.java
@@ -0,0 +1,9 @@
+// Last updated: 11/08/2026, 16:01:02
+class Solution {
+    public int smallestEvenMultiple(int n) {
+        if(n%2!=0){
+         return n*2;
+      }  
+      return n;
+    }
+}
