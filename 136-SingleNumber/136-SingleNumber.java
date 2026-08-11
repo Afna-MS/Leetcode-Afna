4e0422706ee42fe3f517aeccd37b0b813d330fd7
Afna-MS <afnams4050@gmail.com>
2026-08-11 16:14:23 +0530

    Create 136-SingleNumber

---
+++ 136-SingleNumber/136-SingleNumber.java
@@ -0,0 +1,10 @@
+// Last updated: 11/08/2026, 16:14:23
+class Solution {
+    public int singleNumber(int[] nums) {
+        int i,x=0;
+        for(i=0;i<nums.length;i++)
+        x=x^nums[i];
+        return x;
+        
+    }
+}
