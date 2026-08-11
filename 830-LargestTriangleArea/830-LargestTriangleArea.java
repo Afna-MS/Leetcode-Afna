1145310b3b76dbb72026c1d8daf053d0e3538487
Afna-MS <afnams4050@gmail.com>
2026-08-11 16:04:44 +0530

    Create 830-LargestTriangleArea

---
+++ 830-LargestTriangleArea/830-LargestTriangleArea.java
@@ -0,0 +1,19 @@
+// Last updated: 11/08/2026, 16:04:43
+class Solution {
+    public double largestTriangleArea(int[][] points) {
+        double maxArea = 0.0;
+        int n = points.length;
+        for(int i = 0; i<n; i++){
+            for(int j= i+1;j<n;j++){
+                for(int k=j+1;k<n;k++){
+                    double area = area(points[i], points[j], points[k]);
+                    maxArea = Math.max(maxArea, area);
+                }
+            }
+        }
+        return maxArea;
+    }
+    private double area(int[] p1, int[] p2, int[] p3){
+        return Math.abs( p1[0] * (p2[1] - p3[1]) + p2[0] * (p3[1]-p1[1]) + p3[0] * (p1[1] - p2[1])) / 2.0;
+    }
+}
