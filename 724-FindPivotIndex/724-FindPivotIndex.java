65bd89f68451732a8b64d50dcb3ed6e2372ff29b
Afna-MS <afnams4050@gmail.com>
2026-08-11 16:05:57 +0530

    Create 724-FindPivotIndex

---
+++ 724-FindPivotIndex/724-FindPivotIndex.java
@@ -0,0 +1,15 @@
+// Last updated: 11/08/2026, 16:05:57
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int total = 0;
+        for (int x : nums) total += x;
+        int left = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (left == total - left - nums[i]) {
+                return i;
+            }
+            left += nums[i];
+        }
+        return -1;
+    }
+}
