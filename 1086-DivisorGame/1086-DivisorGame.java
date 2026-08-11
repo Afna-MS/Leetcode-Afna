1a1d8840ae54c8e2b4fdec7b2eb3481f8637006c
Afna-MS <afnams4050@gmail.com>
2026-08-11 16:03:11 +0530

    Create 1086-DivisorGame

---
+++ 1086-DivisorGame/1086-DivisorGame.java
@@ -0,0 +1,11 @@
+// Last updated: 11/08/2026, 16:03:11
+class Solution {
+    public boolean divisorGame(int n) {
+       if(n%2==0){
+       return(true);
+       }
+       return(false);
+    
+
+    }
+}
