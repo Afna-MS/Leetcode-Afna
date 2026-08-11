482afd9ad3eaa505f60759a97bf87ac11da06cc3
Afna-MS <afnams4050@gmail.com>
2026-08-11 16:02:50 +0530

    Create 1161-ProjectEmployeesI

---
+++ 1161-ProjectEmployeesI/1161-ProjectEmployeesI.sql
@@ -0,0 +1,6 @@
+-- Last updated: 11/08/2026, 16:02:50
+SELECT project_id,
+       ROUND(AVG(experience_years), 2) AS average_years
+FROM Project
+JOIN Employee USING (employee_id)
+GROUP BY project_id;
