d8115b9acbde2e75dc7e816ad0e7e784ee5167ea
Afna-MS <afnams4050@gmail.com>
2026-08-11 16:10:11 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,9 @@
+// Last updated: 11/08/2026, 16:10:11
+class Solution {
+    public boolean canWinNim(int n){
+        if(n%4==0){
+        return(false);
+        }
+        return(true);
+    }
+}
