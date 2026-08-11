c38a940b18d3b56527255598672ed97f227d2993
Afna-MS <afnams4050@gmail.com>
2026-08-11 16:16:33 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -0,0 +1,16 @@
+// Last updated: 11/08/2026, 16:16:33
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character>st=new Stack<>();
+        for(char c:s.toCharArray()){
+            if(c=='('||c=='{'||c=='[')
+            st.push(c);
+            else{
+              if(st.isEmpty()) return false;
+              char t=st.pop();
+              if(c==')' && t!='('||c=='}' && t!='{'||c==']' && t!='[') return false;
+            }
+        }
+        return st.isEmpty();  
+    }
+}
