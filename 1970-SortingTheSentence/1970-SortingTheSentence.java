bea169642eb1e500e33b3005c7416a266ae2ecd9
Afna-MS <afnams4050@gmail.com>
2026-08-11 16:01:31 +0530

    Create 1970-SortingTheSentence

---
+++ 1970-SortingTheSentence/1970-SortingTheSentence.java
@@ -0,0 +1,14 @@
+// Last updated: 11/08/2026, 16:01:31
+class Solution {
+    public String sortSentence(String s) {
+        String[] words = s.split(" ");
+        String[] res = new String[words.length];
+        
+        for (String word : words) {
+            int pos = word.charAt(word.length() - 1) - '0'; 
+            res[pos - 1] = word.substring(0, word.length() - 1); 
+        }
+        
+        return String.join(" ", res);
+    }
+}
