e750641047f1e928674935a5298172f80dbbf6c0
Afna-MS <afnams4050@gmail.com>
2026-08-11 16:05:02 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.java
@@ -0,0 +1,17 @@
+// Last updated: 11/08/2026, 16:05:02
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0, h = nums.length - 1;
+        while (l <= h) {
+            int mid = l + (h - l) / 2; 
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                h = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
