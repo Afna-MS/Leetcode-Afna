c20e87e7b20d0c2d73a84b328ccef8742b692501
Afna-MS <afnams4050@gmail.com>
2026-08-11 16:10:22 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -0,0 +1,15 @@
+// Last updated: 11/08/2026, 16:10:22
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        for (int i = index; i < nums.length; i++) 
+            nums[i] = 0;
+    }
+}
+
